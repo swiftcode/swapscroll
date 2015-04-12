82de92dd0d272bdf1d80d263ba8c615a51a1dec6
mpc <mcampbell@elevenfiftyconsulting.com>
2015-04-12 13:09:28 -0400

    Initial Commit

---
+++ swapscroll/swapscroll/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  swapscroll
+//
+//  Created by mpc on 4/12/15.
+//  Copyright (c) 2015 mpc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
