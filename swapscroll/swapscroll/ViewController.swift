95acc5c6eca77a012747aba765b5644351feacf4
mpc <mcampbell@elevenfiftyconsulting.com>
2015-04-15 10:58:50 -0400

    First version of program.

--- swapscroll/swapscroll/ViewController.swift
+++ swapscroll/swapscroll/ViewController.swift
@@ -6,14 +6,16 @@
 //  Copyright (c) 2015 mpc. All rights reserved.
 //
 
+import Foundation
 import Cocoa
+import IOKit
 
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        swapScrollDirection()
     }
 
     override var representedObject: AnyObject? {
@@ -22,6 +24,36 @@ class ViewController: NSViewController {
         }
     }
 
-
+    func mouseDetected() -> Bool {
+        var task     = NSTask()
+        var pipe     = NSPipe()
+        var detected = false
+        
+        task.launchPath = "/usr/sbin/ioreg"
+        task.standardOutput = pipe
+        task.launch()
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output: String = NSString(data: data, encoding: NSUTF8StringEncoding)!
+        
+        detected = Regex("mouse").test(output)
+        
+        return detected
+    }
+    
+    func swapScrollDirection() {
+        var task = NSTask()
+        
+        if mouseDetected() {
+            let command = ["-c", "defaults write com.apple.swipescrolldirection -bool false"]
+            task.launchPath = "/bin/bash"
+            task.arguments  = command
+        } else {
+            let command = ["-c", "defaults write com.apple.swipescrolldirection -bool true"]
+            task.launchPath = "/bin/bash"
+            task.arguments  = command
+        }
+        
+        task.launch()
+    }
 }
-
